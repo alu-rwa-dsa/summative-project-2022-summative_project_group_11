a4d9b94c26449edef11e212607ede66523c8892c
Evelyne <e.umubyeyi@alustudent.com>
2022-04-11 22:45:54 +0200

    Adding cards layout on top

--- animation.py
+++ animation.py
@@ -2,8 +2,8 @@ from tkinter import *
 import time
 from PIL import Image, ImageTk
 
-width=1450
-height = 600
+width = 1450
+height = 1000
 root = Tk()
 canvas = Canvas(root, width=width, height=height, bg="green")
 canvas.pack()
@@ -12,35 +12,74 @@ photo1 = PhotoImage(file="images/2_of_hearts.png")
 photo_width = photo.width()
 photo_height = photo.height()
 
-card1 = Image.open("images/2_of_hearts.png")
-card1_size = card1.resize((80, 80))
-card1_main = ImageTk.PhotoImage(card1_size)
-my_image1 = canvas.create_image(0, 10, image=card1_main, anchor=NW)
 
-card2 = Image.open("images/2_of_clubs.png")
-card2_size = card2.resize((80, 80))
-card2_main = ImageTk.PhotoImage(card2_size)
-my_image2 = canvas.create_image(85, 10, image=card2_main, anchor=NW)
+def resize_image(card):
+    open_image = Image.open(card)
+    # resize our image
+    resize_image = open_image.resize((50, 60))
 
-card3 = Image.open("images/3_of_clubs.png")
-card3_size = card3.resize((80, 80))
-card3_main = ImageTk.PhotoImage(card3_size)
-my_image3 = canvas.create_image(170, 10, image=card3_main, anchor=NW)
+    our_card_image = ImageTk.PhotoImage(resize_image)
+    return our_card_image
 
-card4 = Image.open("images/5_of_clubs.png")
-card4_size = card4.resize((80, 80))
-card4_main = ImageTk.PhotoImage(card4_size)
-my_image4 = canvas.create_image(255, 10, image=card4_main, anchor=NW)
 
-card5 = Image.open("images/6_of_clubs.png")
-card5_size = card5.resize((80, 80))
-card5_main = ImageTk.PhotoImage(card5_size)
-my_image5 = canvas.create_image(340, 10, image=card5_main, anchor=NW)
+x_coords = 0
+y_coords = 10
+suits = ["diamonds", "clubs", "hearts", "spades"]
+values = range(2, 15)
+deck = []
+for suit in suits:
+    for value in values:
+        deck.append(f"{value}_of_{suit}")
+x=30
+y=20
+counter = 1
+for i in deck:
+    card = resize_image(f"images/{i}.png")
+    card_label = Label(image = card)
+    card_label.image = card
+    if counter % 13 == 1 and counter != 1:
+        y += 80
+        x = 30
+    card_label.place(x=x,y=y)
+    # card_label.grid(column = index, row = 0)
+    # if x_coords > width:
+    #     my_image2 = canvas.create_image(x_coords, y_coords, image=card, anchor=NW)
+    # canvas.create_image(x_coords, y_coords, image=card, anchor=NW)
+    # x_coords += 85
+    # index +=1
+    x += 70
+    counter +=1
 
-canvas.create_text(10, 600, text="bucket 1", fill="black", font=('Helvetica 15 bold'), anchor=SW)
-canvas.create_text(140, 600, text="bucket 2", fill="black", font=('Helvetica 15 bold'), anchor=SW)
-canvas.create_text(240, 600, text="bucket 3", fill="black", font=('Helvetica 15 bold'), anchor=SW)
-canvas.create_text(380, 600, text="bucket 4", fill="black", font=('Helvetica 15 bold'), anchor=SW)
+# card1 = Image.open("images/2_of_hearts.png")
+# card1_size = card1.resize((80, 80))
+# card1_main = ImageTk.PhotoImage(card1_size)
+# my_image1 = canvas.create_image(0, 10, image=card1_main, anchor=NW)
+#
+# card2 = Image.open("images/2_of_clubs.png")
+# card2_size = card2.resize((80, 80))
+# card2_main = ImageTk.PhotoImage(card2_size)
+# my_image2 = canvas.create_image(85, 10, image=card2_main, anchor=NW)
+#
+# card3 = Image.open("images/3_of_clubs.png")
+# card3_size = card3.resize((80, 80))
+# card3_main = ImageTk.PhotoImage(card3_size)
+# my_image3 = canvas.create_image(170, 10, image=card3_main, anchor=NW)
+#
+# card4 = Image.open("images/5_of_clubs.png")
+# card4_size = card4.resize((80, 80))
+# card4_main = ImageTk.PhotoImage(card4_size)
+# my_image4 = canvas.create_image(255, 10, image=card4_main, anchor=NW)
+#
+# card5 = Image.open("images/6_of_clubs.png")
+# card5_size = card5.resize((80, 80))
+# card5_main = ImageTk.PhotoImage(card5_size)
+# my_image5 = canvas.create_image(340, 10, image=card5_main, anchor=NW)
+
+x_position = 30
+for i in range(1,11):
+    canvas.create_text(x_position, 700, text="bucket"+str(i), fill="black", font=('Helvetica 15 bold'), anchor=SW)
+    x_position += 130
+
 
 x_velocity = 10
 y_velocity = 10
@@ -59,19 +98,22 @@ y_velocity = 10
 #     time.sleep(1)
 #     break
 
-cardlist = [my_image1, my_image2, my_image3, my_image4, my_image5]
+# cardlist = [my_image1, my_image2, my_image3, my_image4, my_image5]
 # for i in range(len(cardlist)):
 #     if i == 0:
 #         canvas.move(cardlist[i], 10, 590-100)
 #         time.sleep(1)
 #     if i == 1:
 #         canvas.move(cardlist[i], 55, 590-100)
-i = 0
-while True:
-    if i == len(cardlist):
-        break
-    canvas.move(cardlist[i], 10, 590-100)
-    root.update()
-    time.sleep(1)
-    i += 1
+# i = 0
+# y = 590-100
+# while True:
+#     if i == len(cardlist):
+#         break
+#     canvas.move(cardlist[i], x_velocity, y)
+#     root.update()
+#     time.sleep(1)
+#     y -= 10
+#     x_velocity -= 85
+#     i += 1
 root.mainloop()
